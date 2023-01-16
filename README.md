fc073cb18680d46df3e95792e5d4cced92175938
Juan Manuel Rodriguez <rodriguezjuanm@live.com>
2023-01-16 14:13:35 -0300

    feat: se agregaron funcionalidades como: poder ingresar el tipo de CPT, y 2 meta_keys; ademas la funcion primero BORRA los post anteriores y luego graba los que existan en el archivo. se agrego una imagen de ejemplo del archivo a subir y un poco de estilos css

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # plugin_import_csv
 Plugin desarrollado para leer un .csv y que guarde en la DB de Wordpress como post los datos que estan dentro del .csv
+
+## Primer paso
+Se debe ingresar en el formulario el slug del CTP y los meta_key que se desea cargar. Tener en cuenta que ésta version del plugin acepta unicamente 2 meta_keys de tipo string.
+
+## Segundo paso
+Cargar el archivo .csv, en caso de enviar otro tipo de archivo da error, y los datos pueden estar separados por ; o ,. 
+
+## Tercer paso
+La funcion intermanete BORRA los post existentes y graba unicamente los subidos en el archivo. Tener ésto en cuenta ya que se puede perder información valiosa.
+
+## Cuarto paso
+Contactarme y realizar una donación :P
--- plugin_import_csv.php
+++ plugin_import_csv.php
@@ -8,9 +8,11 @@ Author: Juan Rodriguez
 Author URI: https://tuweb.com
 License: GPL2
 */
+include('./includes/example_CSV.png');
 
 // Create a top-level menu item in the dashboard
-function csv_importer_menu() {
+function csv_importer_menu()
+{
     add_menu_page(
         'CSV Importer',
         'CSV Importer',
@@ -20,23 +22,83 @@ function csv_importer_menu() {
         'dashicons-upload'
     );
 }
-add_action( 'admin_menu', 'csv_importer_menu' );
+add_action('admin_menu', 'csv_importer_menu');
 
 // Display the page for the plugin
-function csv_importer_page() {
-    echo '<div style="min-height: 100vh; position: relative;" class="wrap">';
-    echo '<h1>CSV Importer</h1>';
-    echo '<div style=""> Plugin desarrollado por <a href="https://icornio.com" target="_blank">iCornio Tech</a> </div>';
-    echo '<hr>';
-    echo '<h2>Subir un archivo .csv</h2>';
-    echo '<form method="post" enctype="multipart/form-data">';
-    echo '<input type="file" name="file">';
-    echo '<input type="submit" name="submit" value="Import">';
-    echo '</form>';
+function csv_importer_page()
+{ ?>
+<style>
+    *{ 
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+.container{
+    width: 100%;
+    padding: 1rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+}
+.col{
+    width: 100%;
+    height: 100%;
+}
+.col_img{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+}
+.col_img img{
+    padding: 2rem;
+    width: 400px;
+    height: auto;
+    border-radius: 10px;
+}
+</style>
+<div style="min-height: 100vh;" class="wrap">
+<h1>CSV Importer</h1>
+<div> Plugin desarrollado por <a href="https://icornio.com" target="_blank">iCornio Tech</a> </div>
+    <hr>
+    <div class="container">
+        <div class="col col_form">
+            <h3>Formulario</h3>
+            <form method="post" enctype="multipart/form-data">
+            <label for="cpt">Ingrese el slug del Custom Post type a cargar</label></br>
+            <input id="cpt" name="cpt_slug" type="text" value="tecnicos" > SISCARD, Si quiere cargar técnicos debe colocar: tecnicos</br>
+            <label for="cf_1">Ingrese el meta_key del primer Custom Field</label></br>
+            <input id="cf_1" name="cf_1" type="text" value="tecnico_dni"> SISCARD, Si quiere cargar técnicos debe colocar: tecnico_dni</br>
+            <label for="cf_2">Ingrese el meta_key del segundo Custom Field</label></br>
+            <input id="cf_2" name="cf_2" type="text" value="tecnico_zona"> SISCARD, Si quiere cargar técnicos debe colocar: tecnico_zona</br>
+            <label for="file">Subir un Archivo .csv</label></br>
+            <input type="file" id="file" name="file"></br>
+            </br>
+            <input type="submit" name="submit" value="Cargar">
+            </form>
+        </div>
+        <div class="col col_img">
+            <h3>Ejemplo del archivo</h3>
+            <img src="<?php echo plugin_dir_url( __FILE__ ) . 'includes/example_CSV.png' ?>" alt="ejemplo">
+        </div>
+    </div>
+</div>
+<?php
+    if (isset($_POST['submit'])) {
+        // Get the post type from the form
+        $post_type = $_POST['cpt_slug'];
 
-    echo '</div>';
+        // Delete all posts with the specified post type
+        $args = array(
+            'post_type' => $post_type,
+            'post_status' => 'any',
+            'numberposts' => -1
+        );
+        $posts = get_posts( $args );
+        foreach ( $posts as $post ) {
+            wp_delete_post( $post->ID, true );
+        }
 
-    if(isset($_POST['submit'])){
+        // Rest of the code for handling the CSV file and creating new posts
         $file = $_FILES['file'];
         $file_name = $file['name'];
         $file_tmp = $file['tmp_name'];
@@ -44,22 +106,26 @@ function csv_importer_page() {
         $file_error = $file['error'];
         $file_ext = explode('.', $file_name);
         $file_ext = strtolower(end($file_ext));
+        $custom_post_type = $_POST["cpt_slug"];
+        $meta_key_1 = $_POST["cf_1"];
+        $meta_key_2 = $_POST["cf_2"];
+
 
         $allowed = array('csv');
 
-        if(in_array($file_ext, $allowed)){
-            if($file_error === 0){
+        if (in_array($file_ext, $allowed)) {
+            if ($file_error === 0) {
 
-                    $file_name_new = uniqid('', true) . '.' . $file_ext;
-                    $upload_dir = wp_upload_dir();
-                    $file_destination = $upload_dir['basedir'].'/plugin_import_csv/'.$file_name_new;
-                    if (!is_dir($upload_dir['basedir'].'/plugin_import_csv/')) {
-                        wp_mkdir_p($upload_dir['basedir'].'/plugin_import_csv/');
-                    }
+                $file_name_new = uniqid('', true) . '.' . $file_ext;
+                $upload_dir = wp_upload_dir();
+                $file_destination = $upload_dir['basedir'] . '/plugin_import_csv/' . $file_name_new;
+                if (!is_dir($upload_dir['basedir'] . '/plugin_import_csv/')) {
+                    wp_mkdir_p($upload_dir['basedir'] . '/plugin_import_csv/');
+                }
 
-                if(move_uploaded_file($file_tmp, $file_destination)){
+                if (move_uploaded_file($file_tmp, $file_destination)) {
                     $delimiter = ';';
-                    if( ($handle = fopen($file_destination, "r")) !== false){
+                    if (($handle = fopen($file_destination, "r")) !== false) {
                         $firstLine = fgets($handle);
                         $semicolonCount = substr_count($firstLine, ';');
                         $commaCount = substr_count($firstLine, ',');
@@ -70,41 +136,48 @@ function csv_importer_page() {
 
                         $row = 1;
                         $count = 0;
-                        
-                            while (($data = fgetcsv($handle, 1000, $delimiter)) !== false) {
-                                if ($row > 1) {
-                                    $proyecto_title = $data[0];
-                                    $proyecto_dni = $data[1];
-                                    
-                                    $post = array(
-                                        'post_title'    => $proyecto_title,
-                                        'post_type'     => 'proyectos',
-                                        'post_status'   => 'publish'
-                                    );
-                        
-                                    $post_id = wp_insert_post($post);
-                                    if($post_id) {
-                                        add_post_meta($post_id, 'proyecto_dni', $proyecto_dni);
-                                        $count++;
-                                    }
 
+                        while (($data = fgetcsv($handle, 1000, $delimiter)) !== false) {
+                            if ($row > 1) {
+                                //CPT debe ser exacto a lo grabado en la BD
+                                $CPT_nombre = $data[0];
+                                //DNI recortado para los ultimos 4 digitos
+                                $string = $data[1];
+                                $string = trim($string);
+                                $CF_1 = substr($string, -4);
+                                //Zona, string sin limites
+                                $CF_2 = $data[2];
+
+                                $post = array(
+                                    'post_title'    => $CPT_nombre,
+                                    'post_type'     => $custom_post_type,
+                                    'post_status'   => 'publish'
+                                );
+
+                                $post_id = wp_insert_post($post);
+                                if ($post_id) {
+                                    add_post_meta($post_id, $meta_key_1, $CF_1);
+                                    add_post_meta($post_id, $meta_key_2, $CF_2);
+                                    $count++;
                                 }
-                                $row++;
+
                             }
-                            fclose($handle);
-                        
+                            $row++;
+                        }
+                        fclose($handle);
+
                         echo '<div class="updated notice is-dismissible"> <p>Archivo importado correctamente!</p> 
-                        <h3> Filas afectadas='. $count .'</h3>
+                        <h3> Filas afectadas=' . $count . '</h3>
                         </div>';
-                    }else{
+                    } else {
                         echo '<div class="error notice is-dismissible"> <p>Juan: An error occurred while processing the CSV file.</p> </div>';
                     }
-                }else{
-                    echo '<div class="error notice is-dismissible"> <p>No se guarda el archivo.</p> </div>'; 
+                } else {
+                    echo '<div class="error notice is-dismissible"> <p>No se guarda el archivo.</p> </div>';
                 }
             }
         } else {
             echo '<div class="error notice is-dismissible"> <p>Solo se permiten archivos con formato .csv</p> </div>';
         }
-        }
-        }
+    }
+}
